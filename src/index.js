6be469f993cde8cc064a848def4fa1c385091076
HenryToti <henryokoth3@gmail.com>
2021-04-19 12:57:35 +0300

    1.Add babel to project.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
-const elevenShieldRecipe = {
-    leatherStripe:2,
-    ironIngot:1,
-    refinedMoonstone:4
-}
+const elevenShieldRecipe = 
+    {
+        leatherStripe:2,
+        ironIngot:1,
+        refinedMoonstone:4
+    }
+const twelveShieldRecipe = {
+    ...elevenShieldRecipe,
+    leather:9,
+    refinedMoonstone:30
+};
+
 
 console.log(elevenShieldRecipe);
+console.log(twelveShieldRecipe);
